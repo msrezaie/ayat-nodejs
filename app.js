8c2cd778e8c1aa4b4bd63358b574cc000302c33e
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-05-03 22:10:55 -0400

    filtered paths, and 404 page added
    paths to admin page and main pages are separated into their own routes files, static files are served using express.static, a 404 route has been added

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+const path = require('path');
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    res.send("<h1>welcome</h1>");
+const mainRoutes = require('./routes/main');
+const adminRoutes = require('./routes/admin');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+app.use(adminRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(5000);
