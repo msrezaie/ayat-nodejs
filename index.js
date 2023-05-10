05985ea41131c87ea8649d9edc84185cab97e8dc
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-05-10 12:30:03 -0400

    configured files for vercel deployment

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ app.use((req, res, next) => {
   res.status(404).render("404");
 });
 
-app.listen(4000);
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Server running in PORT ${PORT}`));
